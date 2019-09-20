f9022e88625d48c029a1814f367e1b81ea8f89f5
Anatolna <taotoj@gmail.com>
2019-09-20 17:54:00 +0300

    Moved API_KEY and PROXY to settings.py

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
 import logging
+import settings
+
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
@@ -14,15 +12,15 @@ def main():
     ...
 
 def main():
-    mybot = Updater('930126622:AAFwRyYNrTOlu0UMDFSzK9wGf34qAuRHRBQ', request_kwargs=PROXY)
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
 
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp = mybot.dispatcher#диспетчер принимает входящие и раскидывает по получателям
+    dp.add_handler(CommandHandler("start", greet_user))#CommandHandler - обработчик команд
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))#MessageHandler - обработчик сообщений, Filters показывает, с каким типом сообщений мы хотим работать
 
 
-    mybot.start_polling()
-    mybot.idle()
+    mybot.start_polling()#mybot, начни регул.ходить на платформу телеграм и проверяй наличие сообщений
+    mybot.idle()# mybot будет работать, пока мы его принудительно не остановим
 
 def greet_user(bot, update):
     print('Вызван /start')
@@ -34,4 +32,4 @@ def talk_to_me(bot, update):
     print(user_text)
     update.message.reply_text(user_text)
 
-main()
+main()# вызов функции
